ad916171881e82c5f68ad1a9bc064becfa0541da
action <action@github.com>
2025-04-14 09:09:22 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,62 +1,220 @@
-//Thu Apr 03 2025 22:58:58 GMT+0000 (Coordinated Universal Time)
+//Mon Apr 14 2025 09:09:22 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function main(_0x43d166) {
-  const _0xe07ae7 = _0x43d166.url,
-    _0x16d91c = ku9.getQuery(_0xe07ae7, "key"),
-    _0x227698 = ku9.getQuery(_0xe07ae7, "cid"),
-    _0x3278f1 = ku9.getQuery(_0xe07ae7, "playseek");
-  if (_0xe07ae7.includes("/k-Web/")) {
-    return;
-  }
-  let _0xc45281 = "http://live.88688.top/hostip.php",
-    _0x1de9a5 = ku9.getCache("itvToken"),
-    _0x1f9b1d = _0xa51c8f(_0x1de9a5);
-  if (_0x1f9b1d === 0 || _0x1f9b1d === 1) {
-    {
-      _0x1de9a5 = ku9.get(_0xc45281, null);
-      var _0x159e37 = ku9.setCache("itvToken", _0x1de9a5, 2592000000);
+function SourceOfTheIssue() {
+  var _0x457623 = document.currentScript;
+  var _0x263e0c = _0x457623.src.split("?");
+  var _0x50723a = new Object();
+  if (_0x263e0c.length > 1) {
+    var _0x20d814 = _0x263e0c[1].split("-");
+    for (var _0x43d57e = 0; _0x43d57e < _0x20d814.length; _0x43d57e++) {
+      var _0x1ddf8c = _0x20d814[_0x43d57e].split("=");
+      _0x50723a[_0x1ddf8c[0]] = _0x1ddf8c[1];
     }
   }
-  if (_0x3278f1 !== null) {
+  return _0x50723a;
+}
+var mids,
+  killing_formalism,
+  we_killing_formalism,
+  We_re_all_cowards,
+  ISay = "are we anti-socialists? against the government? spies? traitor? fresh blood? no, we are just weak, sick bystanders; we just need to rot little by little with formalism until we finally become food for new shoots. ",
+  RottenUs = SourceOfTheIssue();
+for (var ADropInTheSea in RottenUs) {
+  if (ADropInTheSea == "mid") mids = RottenUs[ADropInTheSea];else ADropInTheSea == "nichts" ? We_re_all_cowards = RottenUs[ADropInTheSea] : (we_killing_formalism = ADropInTheSea, killing_formalism = RottenUs[ADropInTheSea]);
+}
+var CurseOfTheGoldenFlower = {};
+CurseOfTheGoldenFlower[we_killing_formalism] = killing_formalism;
+function OneWayTicket(_0x43c6b4) {
+  StartKilling(function () {
     {
-      const _0x426953 = _0x3278f1.split("-"),
-        _0x3ca690 = _0x4acb47(_0x426953[0]),
-        _0x4f7516 = _0x4acb47(_0x426953[1]);
-      let _0x5e6b84 = "http://gslbserv.itv.cmvideo.cn:80/" + _0x16d91c + "/1.m3u8?channel-id=" + _0x227698 + "&Contentid=" + _0x16d91c + "&livemode=4&stbId=IPTV&starttime=" + _0x3ca690 + "&endtime=" + _0x4f7516,
-        _0x97711e = {
-          "Content-Type": "application/x-www-form-urlencoded",
-          "User-Agent": "okhttp/3.12.11"
+      var _0x64940b = [],
+        _0x32a804 = document.querySelectorAll("#btngroup .playbtn");
+      _0x32a804.forEach(_0xed5551 => {
+        _0x64940b.push({
+          "src": _0xed5551.dataset.src,
+          "id": _0xed5551.dataset.id,
+          "mid": _0xed5551.dataset.mid,
+          "sname": _0xed5551.dataset.sname,
+          "stype": _0xed5551.dataset.stype,
+          "subf": _0xed5551.dataset.subf
+        });
+      });
+      _0x43c6b4(_0x64940b);
+    }
+  });
+}
+var u = navigator.userAgent,
+  app = navigator.appVersion,
+  isAndroid = u.indexOf("Android") > -1 || u.indexOf("Linux") > -1,
+  isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+  Rain = isdbime(u);
+function IwasKing(_0x28775f, _0x153154) {
+  if (_0x28775f.src.indexOf("https://") == -1 && _0x28775f.src.indexOf("http://") == -1) {
+    var _0x1bbe05 = atob(_0x28775f.src),
+      _0x1cedd3 = new Uint8Array(_0x1bbe05.length);
+    for (let _0x20093f = 0; _0x20093f < _0x1bbe05.length; _0x20093f++) {
+      _0x1cedd3[_0x20093f] = _0x1bbe05.charCodeAt(_0x20093f);
+    }
+    _0x28775f.src = _0x350ef8027(window[_0x350ef8027(window.DimGive("8069", mids, decodeURIComponent(killing_formalism)))](_0x1cedd3, mids, decodeURIComponent(killing_formalism)));
+  }
+  if (_0x28775f.src.substring(_0x28775f.src.lastIndexOf("."), _0x28775f.src.length) == ".m3u8") {
+    if (isAndroid) pobj = {
+      "src": _0x28775f.src,
+      "type": "application/vnd.apple.mpegurl"
+    }, _0x153154(pobj);else isiOS ? ($.ajaxSettings.async = false, $.get(_0x28775f.src, function (_0x369bfb) {
+      var _0x452ca9 = "data:application/vnd.apple.mpegurl;base64," + btoa(_0x369bfb);
+      pobj = {
+        "src": _0x452ca9,
+        "type": "application/x-mpegurl"
+      };
+      _0x153154(pobj);
+    })) : (pobj = {
+      "src": _0x28775f.src,
+      "type": "application/vnd.apple.mpegurl"
+    }, _0x153154(pobj));
+  } else {
+    if (_0x28775f.src.substring(_0x28775f.src.lastIndexOf("."), _0x28775f.src.length) == ".gazes_v-@_info" || _0x28775f.src.indexOf("+gazes_v-@_info") != -1) TheBigShip(_0x28775f.src, function (_0x1cecc8) {
+      if (isiOS) {
+        var _0x4530f1 = "data:application/vnd.apple.mpegurl;base64," + btoa(_0x1cecc8);
+        pobj = {
+          "src": _0x4530f1,
+          "type": "application/vnd.apple.mpegurl"
+        };
+        _0x153154(pobj);
+      } else {
+        {
+          var _0x253d44 = new Blob([window.allnothing + _0x1cecc8], {
+            "type": "image/png"
+          });
+          pobj = {
+            "src": window.URL.createObjectURL(_0x253d44),
+            "type": "application/vnd.apple.mpegurl"
+          };
+          _0x153154(pobj);
+        }
+      }
+    });else {
+      if (_0x28775f.src.substring(_0x28775f.src.lastIndexOf("."), _0x28775f.src.length) == ".nothing") {
+        {
+          if (isiOS) {
+            $.ajaxSettings.async = false;
+            $.get(_0x28775f.src, function (_0xf89b86) {
+              {
+                var _0x24043d = "data:application/dash+xml;base64," + btoa(_0xf89b86);
+                pobj = {
+                  "src": _0x24043d,
+                  "type": "application/dash+xml"
+                };
+                _0x153154(pobj);
+              }
+            });
+          } else _0x153154({
+            "src": _0x28775f.src,
+            "type": "application/dash+xml"
+          });
+        }
+      } else _0x28775f.src.indexOf("cloud.189.cn") != -1 ? $.post({
+        "url": "/fetch_189c_murl",
+        "headers": CurseOfTheGoldenFlower,
+        "data": {
+          "urls": _0x28775f.src,
+          "mid": mids
         },
-        _0x128ffb = ku9.request(_0x5e6b84, "GET", JSON.stringify(_0x97711e), null, false),
-        _0x357386 = _0x128ffb.headers.Location;
-      return JSON.stringify({
-        "url": _0x357386,
-        "host": "39.135.132.221"
+        "success": function (_0x1fdb21) {
+          _0x153154({
+            "src": _0x1fdb21.url,
+            "type": "video/mp4"
+          });
+        }
+      }) : _0x153154({
+        "src": _0x28775f.src,
+        "type": "video/mp4"
       });
     }
-  } else {
-    let _0xfa4da8 = "http://gslbserv.itv.cmvideo.cn:80/" + _0x16d91c + "/1.m3u8?channel-id=" + _0x227698 + "&Contentid=" + _0x16d91c + "&livemode=1&stbId=IPTV",
-      _0x1f302b = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent": "okhttp/3.12.11"
-      },
-      _0x5292c4 = ku9.request(_0xfa4da8, "GET", JSON.stringify(_0x1f302b), null, false),
-      _0x18ed12 = _0x5292c4.headers.Location;
-    return JSON.stringify({
-      "url": _0x18ed12,
-      "host": "39.135.132.221"
-    });
   }
-  function _0xa51c8f(_0x396c3d) {
-    if (!_0x396c3d) return 0;
-  }
-  function _0x4acb47(_0x43ed9f) {
+  $(".vjs-big-play-button").addClass("vjs-loaded-icon");
+}
+var byteArray = [];
+function TheBigShip(_0xd57642, _0x113144) {
+  var _0xced9f8 = new XMLHttpRequest();
+  _0xced9f8.open("get", _0xd57642, false);
+  var _0x53aca9 = _0xd57642.split("/").pop();
+  _0xced9f8.overrideMimeType("text/plain; charset=x-user-defined");
+  _0xced9f8.onload = function () {
     {
-      const _0x256f31 = _0x43ed9f.slice(0, 8),
-        _0x267498 = _0x43ed9f.slice(8),
-        _0x483cba = _0x256f31 + "T" + _0x267498 + ".00Z";
-      return _0x483cba;
+      if (this.status == 200) {
+        var _0x371720 = this.response;
+        byteArray = [];
+        for (var _0x409709 = 0; _0x409709 < _0x371720.length; ++_0x409709) {
+          byteArray.push(_0x371720.charCodeAt(_0x409709) & 255);
+        }
+        var _0x2cc4b1 = new Uint8Array(byteArray),
+          _0x4fce6e = pako.inflate(_0x2cc4b1, {
+            "to": "string"
+          }),
+          _0x3fc1ef = decodeURIComponent(_0x53aca9),
+          _0x33025c = md5(_0x3fc1ef + "- " + ISay),
+          _0x1bc9fc = CryptoJS.enc.Base64.parse(_0x4fce6e);
+        byteArray = [];
+        for (let _0x4fcc96 = 0; _0x4fcc96 < _0x1bc9fc.sigBytes; _0x4fcc96++) {
+          byteArray[_0x4fcc96] = _0x1bc9fc.words[_0x4fcc96 >>> 2] >>> 24 - _0x4fcc96 % 4 * 8 & 255;
+        }
+        _0x2cc4b1 = new Uint8Array(byteArray);
+        var _0x2b5f59 = window[_0x350ef8027(window.DimGive("nihilism", mids, decodeURIComponent(killing_formalism)))](_0x2cc4b1, _0x33025c, mids, decodeURIComponent(killing_formalism)),
+          _0x4c6cbc = pedantic_tradition.servility(_0x2b5f59);
+        byteArray = [];
+        _0x2cc4b1 = [];
+        _0x113144(_0x350ef8027(_0x4c6cbc));
+      } else (this.status == 404 || this.status == 403) && (player.error({
+        "code": 404,
+        "message": "(" + this.status + ")资源丢失，见谅晚点修复",
+        "type": "MEDIA_ERR_SRC_NOT_SUPPORTED",
+        "dismiss": false
+      }), $.post("/video_err_log", {
+        "title": "404",
+        "mid": window.mid,
+        "url": _0xd57642
+      }, function (_0x11cc80) {}));
     }
+  };
+  _0xced9f8.send();
+}
+var WASM_URL = "https://oos-cn.ctyunapi.cn/allnothing/static/IceDespair-V2.7.1.svg",
+  wasm;
+function StartKilling(_0x2c0931) {
+  var _0x5de8e0 = new Go();
+  fetch(WASM_URL, {
+    "method": "GET",
+    "headers": {
+      "Content-Type": "application/wasm"
+    }
+  }).then(_0x894789 => {
+    return _0x894789.clone().arrayBuffer();
+  }).then(_0x1657fa => {
+    return pedantic_tradition.servility(new Uint8Array(_0x1657fa));
+  }).then(_0x24b8f6 => {
+    return WebAssembly.instantiate(_0x24b8f6, _0x5de8e0.importObject);
+  }).then(_0x40905e => {
+    wasm = _0x40905e.instance;
+    _0x5de8e0.run(wasm);
+    _0x2c0931();
+  }).catch(_0x4b25b7 => {
+    console.error("Error loading wasm:", _0x4b25b7);
+  });
+}
+function _0x350ef8027(_0x13414a) {
+  var _0x3932e7 = "";
+  for (let _0x4f3baa = 0; _0x4f3baa < _0x13414a.length; _0x4f3baa++) {
+    _0x3932e7 += String.fromCharCode(_0x13414a[_0x4f3baa]);
   }
+  return _0x3932e7;
+}
+function md5(_0x982d5) {
+  return CryptoJS.MD5(_0x982d5).toString().substring(8, 24);
+}
+function isdbime(_0x4b947a) {
+  var _0x417bdd = /(iOS|Mac)/,
+    _0x4db1a0 = new RegExp(_0x417bdd.source + ".*Edg");
+  return _0x4db1a0.test(_0x4b947a);
 }
